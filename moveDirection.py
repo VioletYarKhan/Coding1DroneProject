bb9968cf500d1f8dbf473c6c44862d9521dfb990
VioletYarKhan <nicholasyarkhan@gmail.com>
2024-04-24 15:40:05 -0400

    resetPose added

--- moveDirection.py
+++ moveDirection.py
@@ -5,17 +5,21 @@ from time import sleep
 eggbert = tello.Tello()
 eggbert.connect()
 print("Battery: " + str(eggbert.get_battery()))
-
-
+i = 0
 
 
 eggbert.takeoff()
-eggbert.send_rc_control(0, 50, 0, 0)
-sleep(2)
-eggbert.send_rc_control(0, 0, 0, 180)
-sleep(2)
-eggbert.send_rc_control(0, 100, 0, 0)
-sleep(7)
-eggbert.send_rc_control(0, 0, 0, 0)
-eggbert.land()
 
+eggbert.move_down(50)
+while (i < 3):
+    eggbert.move_forward(50)
+    eggbert.rotate_counter_clockwise(60)
+    i += 1
+
+eggbert.rotate_counter_clockwise(60)
+eggbert.move_forward(16)
+eggbert.move_right(30)
+eggbert.move_forward(16)
+eggbert.move_left(30)
+
+eggbert.land()
